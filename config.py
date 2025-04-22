2239f9a1cc5931d5a0bd69cad253598b4f3bd98b
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-04-22 06:17:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8037937173:AAF83Xwawt7ZhrpH92ABsQyrp9tuUCy4zYg")
+API_ID = int(os.environ.get("API_ID", "22403100"))
+API_HASH = os.environ.get("API_HASH", "ccbc3f662735abfa604ef6309ba76e67")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+OWNER_ID = int(os.environ.get("OWNER_ID", "7950514048"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://sumitsajwan135:gameno01@cluster0.ja0i0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "FileShare")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "300")) # auto delete in seconds
 
 
 PORT = os.environ.get("PORT", "8080")
@@ -48,7 +48,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 
 DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
 
-BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
+BOT_STATS_TEXT = "<b>⟦ BOT UPTIME ⟧</b>\n⮞ {uptime}"
 
 
 
@@ -56,9 +56,12 @@ BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
 
 
-USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
+USER_REPLY_TEXT = "<b>✖ DO NOT MESSAGE ME DIRECTLY</b>\n<b>I'M A FILE SHARE BOT ONLY!</b>"
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
+START_MSG = os.environ.get(
+    "START_MESSAGE",
+    "⟦ Hello {mention} ⟧\n\nI can store your private files in a secure channel.\nOthers can access them using a unique link.\n\n➤ Just send me a file to begin!"
+)
 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
 
